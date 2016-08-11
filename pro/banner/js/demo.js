f2bead5795516fedccdbfa58fb781ee7368fbc0c
yizipei <605188854@qq.com>
2016-08-11 17:56:49 +0800

    8.11

--- pro/banner/js/demo.js
+++ pro/banner/js/demo.js
@@ -1,7 +1,13 @@
 function Banner(option){
 	var position = option.position,
 		option = option.option,
-		arrAd;
+		adLen = option.length,
+		arrAd,
+		arrButton,
+		previousIndex = adLen -1,
+		currentIndex = 0,
+		nextIndex = 1,
+		bannerWidth;
 	function createBanner(){
 		var fragment = document.createDocumentFragment();
 		arrAd = option.map(function(list,index){
@@ -10,24 +16,83 @@ function Banner(option){
 			ad.href = list.anchorHref;
 			ad.style.backgroundImage = "url(" + list.imageUrl + ")";
 			fragment.appendChild(ad);
+			var startX,
+				endX,
+				distance,
+				direction;
+			ad.addEventListener("touchstart",function(e){
+				startX = e.touches[0].clientX;
+			},0);
+			ad.addEventListener("touchmove",function(e){
+				nextIndex = currentIndex > adLen - 2 ? 0 : currentIndex + 1;
+				distance = e.touches[0].clientX - startX;
+				direction = distance > 0;
+				this.style.left = distance + "px";
+				if (direction) {
+					arrAd[previousIndex].style.left = distance - bannerWidth + "px";
+				}else{
+					arrAd[nextIndex].style.left = distance + bannerWidth + "px";
+				}
+			},0);
+			ad.addEventListener("touchend",function(e){
+				endX = e.changedTouches[0].clientX;
+				if (Math.abs(distance) > bannerWidth / 2) {
+					this.classList.add(direction ? "ltr" : "rtl");
+				}else{
+					this.classList.add("init");
+				}
+			},0);
+			ad.addEventListener("animationend",function(){
+				this.style.left = null;
+				this.classList.remove("init");
+			},0);
 			return ad;
 		});
 		position.appendChild(fragment);
 	}
+	function createIndicator(){
+		var indicator = document.createElement("div");
+		indicator.className = "indicator";
+		arrButton = option.map(function(list,index){
+			var button = document.createElement("em");
+			button.appendChild(document.createTextNode(index + 1));
+			indicator.appendChild(button);
+			button.addEventListener("touchend",function(){
+				setIndex(index);
+				this.classList.add("current");
+			},0);
+			return button;
+		});
+		arrButton[0].classList.add("current");
+		position.appendChild(indicator);
+	}
+	function setIndex(index){
+		previousIndex = currentIndex;
+		currentIndex = index;
+		setView();
+	}
 	function autoChange(){
-		var index = 0,
-			previousIndex = option.length -1 ;
+		var adLen = option.length;
+		previousIndex = adLen -1 ;
 		setInterval(function(){
-			index= index < option.length-1? index+1 : 0 ;
-			previousIndex = index> 0 ? index-1 : option.length - 1;
-			arrAd[previousIndex].classList.add("previous");
-			arrAd[previousIndex].classList.remove("current");
-			arrAd[index].classList.remove("previous");
-			arrAd[index].classList.add("current");
+			currentIndex= currentIndex < adLen-1? currentIndex + 1 : 0 ;
+			previousIndex = currentIndex> 0 ? currentIndex-1 : adLen - 1;
+			setView();
 		},2000);
 	}
+	function setView(){
+		console.log(previousIndex);
+		arrAd[previousIndex].classList.remove("current");
+		arrAd[previousIndex].classList.add("previous");
+		arrAd[currentIndex].classList.remove("previous");
+		arrAd[currentIndex].classList.add("current");
+		arrButton[previousIndex].classList.remove("current");
+		arrButton[currentIndex].classList.add("current");
+	}
 	createBanner();
-	autoChange();
+	bannerWidth = arrAd[0].offsetWidth;
+	// autoChange();
+	createIndicator();
 }
 var banner = document.querySelector(".banner");
 ajax({
